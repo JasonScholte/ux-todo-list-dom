07606e027024e7570f5b064b077c52545ba87c00
JasonScholte <56888977+JasonScholte@users.noreply.github.com>
2020-08-11 13:43:11 -0500

    Done, Thanks for the help everyone

--- src/index.js
+++ src/index.js
@@ -21,11 +21,36 @@ let todos = [
 function renderTodoApp() {
     let app = document.querySelector("#app")
     let h1 = document.createElement("h1")
-    // your code here
+
+
+    // my code
+let todosList = document.createElement("ul");
+todosList.classList.add("todoing");
+
 
     h1.innerText = "Todo List"
 
     app.appendChild(h1)
+
+// my code
+    document.body.append(todosList);
+
+
+    for (let i=0; i< todos.length; i++){
+        let todo = todos[i];
+        let completed = todo.completed;
+
+        let todosMark = `
+            <li class="todo-list">
+                <input type="checkbox" checked></input>
+                 <p>${todo.completed}</p>
+                 <p>${todo.description}</p>
+            </li>
+        `
+
+        todosList.innerHTML += todosMark;
+        
+    }
     // and maybe some here
 }
 
